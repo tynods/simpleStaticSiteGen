119327d7fdb8a2a4dc9ac474651f409383f2e45f
tynods <githb@tynods.net>
2025-05-30 11:05:00 +0200

    ready to serve online

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import shutil
 import re
@@ -26,7 +27,7 @@ def extract_title(markdown):
     return m.group(1).strip()
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     f = open(from_path)
     from_content = f.read()
@@ -35,25 +36,26 @@ def generate_page(from_path, template_path, dest_path):
     html = markdown_to_html_node(from_content).to_html()
     title = extract_title(from_content)
     template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     f_out = open(dest_path, "w")
     f_out.write(template)
     f_out.close()
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for elem in os.listdir(dir_path_content):
         p_elem = os.path.join(dir_path_content, elem)
         if os.path.isfile(p_elem):
-            generate_page(p_elem, template_path, os.path.join(dest_dir_path, elem.split(".")[0]+".html"))
+            generate_page(p_elem, template_path, os.path.join(dest_dir_path, elem.split(".")[0]+".html"), basepath)
         else :
             p_dto_elem = os.path.join(dest_dir_path, elem)
-            generate_pages_recursive(p_elem, template_path, p_dto_elem)
+            generate_pages_recursive(p_elem, template_path, p_dto_elem, basepath)
 
 
-def clean_public():
-    for elem in os.listdir("public"):
-        p_elem = os.path.join("public", elem)
+def clean_docs():
+    for elem in os.listdir("docs"):
+        p_elem = os.path.join("docs", elem)
         if os.path.isfile(p_elem):
             os.remove(p_elem)
         else:
@@ -61,10 +63,14 @@ def clean_public():
 
 
 
-def main():
-    clean_public()
-    copy_to("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+def main(basepath="/"):
+    clean_docs()
+    copy_to("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+
 
 if __name__=="__main__" :
-    main()
+    basepath = "/"
+    if len(sys.argv)>1:
+        basepath = sys.argv[1]
+    main(basepath)
